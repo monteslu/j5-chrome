665f4da925260213a5c9c32ecb3f2d4c775567ed
Luis Montes <luis@iceddev.com>
2014-12-04 06:48:22 -0700

    update example script

--- views/editor.jsx
+++ views/editor.jsx
@@ -7,17 +7,18 @@ require('codemirror/lib/codemirror.css');
 require('codemirror/theme/monokai.css');
 
 var DEFAULT_SCRIPT = '/* \n'
-  + ' This script is executed when johnny-five is connected\n\n'
   + ' You have the following variables availaible to your script:\n'
   + '   five  = The full johnny-five API !\n'
-  + '   board = this connected johnny-five instance\n'
   + '   io    = the firmata instance for the board\n'
   + '   $     = jQuery, because you might already know jQuery\n'
   + '   _     = lodash, because lodash is awesome\n'
-  + '*/ \n\n\n'
-  + '// Default to pin 13\n'
-  + 'var led = new five.Led(13);\n'
-  + 'led.blink();';
+  + '*/ \n\n'
+  + 'var board = new five.Board({io: io});\n\n'
+  + 'board.on(\'ready\', function(){\n'
+  + '  // Default to pin 13\n'
+  + '  var led = new five.Led(13);\n'
+  + '  led.blink();\n'
+  + '});';
 
 
 var Editor = React.createClass({
